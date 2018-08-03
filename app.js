42cfe39c800e9e9248e3ab5fdd6e784942392f84
shakti iyersingh <shakti.iyersingh@travelyaari.com>
2018-08-03 18:36:03 +0530

    header

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
   next(createError(404));
 });
 
