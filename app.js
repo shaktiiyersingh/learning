ff97a990d63df797555aae45c77f027f01f7f3c8
shakti iyersingh <shakti.iyersingh@travelyaari.com>
2018-08-31 16:54:11 +0530

    sig image

--- app.js
+++ app.js
@@ -13,9 +13,11 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'html');
 //app.set('view engine', 'jade');
 
 //app.use(logger('dev'));
+app.use(bodyParser.json({ 'type': '*/*',limit: '20mb' }));
 app.use(express.static('data/images'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,6 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -48,8 +48,8 @@ router.get('/viewimage', function(req, res) {
 });
 
 router.get('/drawsignature', function(req, res) {
- console.log(req.query.userid);
-  var userid = req.query.userid;
+// console.log(req.query.userId);
+  var userId = req.query.userId;
   var MongoClient = mongodb.MongoClient;
   var url = 'mongodb://monuser:monpass999@ds245150.mlab.com:45150/mondb';
   MongoClient.connect(url,function(err,client){
@@ -57,7 +57,7 @@ router.get('/drawsignature', function(req, res) {
     else{
     var db = client.db('mondb');
    var collection = db.collection('dots');
-   collection.find({},{ userID: userid }).toArray(function(err,result){
+   collection.find({},{ userId: userId }).toArray(function(err,result){
      if(err){}
      else {
       return res.send(result);
@@ -70,12 +70,55 @@ router.get('/drawsignature', function(req, res) {
   });
 });
 
+function decodeBase64Image(dataString) {
+  var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+    response = {};
+
+  if (matches.length !== 3) {
+    return new Error('Invalid input string');
+  }
+
+  response.type = matches[1];
+  response.data = new Buffer(matches[2], 'base64');
+
+  return response;
+}
+
 router.post('/checklogin', function(req, res) {
 
  
 if(req.body.uname === "yo" && req.body.password === "yo"  ){res.redirect('/viewsignature');}
 else{res.redirect('/');}
   
+});
+
+router.post('/addSignatureImage', function(req, res) {
+var userId = req.body.userId;
+  
+  /*
+  var decodedImg = decodeBase64Image(req.body.image);
+ var imageBuffer = decodedImg.data;
+ var type = decodedImg.type;
+ var extension = mime.extension(type);
+ var fileName =  "mytravelsignature." + extension;
+ try{
+       fs.writeFileSync("../sampsite/data/images/" + fileName, imageBuffer, 'utf8');
+    }
+ catch(err){
+    console.error(err)
+ }
+ */
+  //var Buffer = new Buffer(data, 'base64'); 
+  
+ var image = decodeBase64Image(req.body.image);
+ 
+  fs.writeFile("../sampsite/data/images/mytravelsignature.png", image.data, function(err) {
+    console.log(err);
+  });
+  
+  
+  console.log(image);
+  return res.send({"userId":userId,"image":image});
 });
 
 router.post('/addDot', function(req, res) {
@@ -86,7 +129,7 @@ router.post('/addDot', function(req, res) {
     if(err){console.log('connection not happening');}
     else{
     var db = client.db('mondb');
-    var dot = {lat:req.body.lat, lng:req.body.lng};
+    var dot = {lat:req.body.lat, lng:req.body.lng,name:req.body.name};
     db.listCollections().toArray(function(err, names) {
       if(names.includes("dots"))
       {
